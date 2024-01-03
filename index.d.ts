a26339e1d7067e28810a0bfc28d86f2132c537ee
Kyle <kyle@kylerich.dev>
2024-01-02 21:56:53 -0500

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,8 @@
 declare module "@imkylecat/codemirror/mode/zig/zig" {
-    export {};
+    import { Mode } from "codemirror";
+
+  interface ZigMode extends Mode<any> {};
+
+  const ZigMode: ZigMode;
+  export = ZigMode;
 };
